89409769653b95dfbb7907e9ed3e4b0eac697ec0
Enrico Vianello <enrico.vianello@cnaf.infn.it>
2024-11-13 16:07:17 +0100

    Fix test.vo.2

--- manifests/testvo2.pp
+++ manifests/testvo2.pp
@@ -3,9 +3,9 @@ class voms::testvo2 {
   voms::vo { 'test.vo.2':
     servers => [
       {
-        server => 'vgrid02.cnaf.infn.it',
-        port   => 15001,
-        dn     => '/DC=org/DC=terena/DC=tcs/C=IT/ST=Roma/O=Istituto Nazionale di Fisica Nucleare/CN=vgrid02.cnaf.infn.it',
+        server => 'meteora.cloud.cnaf.infn.it',
+        port   => 15003,
+        dn     => '/DC=org/DC=terena/DC=tcs/C=IT/ST=Roma/O=Istituto Nazionale di Fisica Nucleare/CN=meteora.cloud.cnaf.infn.it',
         ca_dn  => '/C=NL/O=GEANT Vereniging/CN=GEANT eScience SSL CA 4',
       },
     ],
