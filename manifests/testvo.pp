30066192b754cca1b9cd45c84c5a81e1353431de
enricovianello <maverick17586@gmail.com>
2025-05-29 22:40:38 +0200

    Restore test.vo for voms-dev

--- manifests/testvo.pp
+++ manifests/testvo.pp
@@ -8,6 +8,12 @@ class voms::testvo {
         dn     => '/DC=org/DC=terena/DC=tcs/C=IT/ST=Roma/O=Istituto Nazionale di Fisica Nucleare/CN=meteora.cloud.cnaf.infn.it',
         ca_dn  => '/C=NL/O=GEANT Vereniging/CN=GEANT eScience SSL CA 4',
       },
+      {
+        server => 'voms-dev.cloud.cnaf.infn.it',
+        port   => 15004,
+        dn     => '/DC=org/DC=terena/DC=tcs/C=IT/ST=Roma/O=Istituto Nazionale di Fisica Nucleare/CN=voms-dev.cloud.cnaf.infn.it',
+        ca_dn  => '/C=NL/O=GEANT Vereniging/CN=GEANT eScience SSL CA 4',
+      },
     ],
   }
 }
